f731396c034ffc352371890efbb6193df925d06b
DexterDimatao <sonicdexter12@gmail.com>
2024-11-18 13:20:41 +0800

    Added fetchData

--- ChartCreator.js
+++ ChartCreator.js
@@ -4,3 +4,17 @@ class ChartCreator {
       this.barCtx = document.getElementById("barChart");
       this.pieCtx = document.getElementById("pieChart");
     }
+    async fetchData() {
+        try {
+          const response = await fetch(this.dataUrl);
+          if (!response.ok) {
+            throw new Error("Network response was not ok " + response.statusText);
+          }
+          return await response.json();
+        } catch (error) {
+          console.error(
+            "There has been a problem with your fetch operation:",
+            error
+          );
+        }
+      }
